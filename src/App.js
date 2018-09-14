eb066976a87af8975fd001ff0db72e959d670d54
MyPro <brookbirch@gmail.com>
2018-09-14 08:58:36 -0500

    added toggle component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 // import PostList from './posts/PostList';
-import FormsAndInputs from './screens/FormsAndInputs';
+// import Toggle from './screens/ToggleRenderProps';
+// import FormsAndInputs from './screens/FormsAndInputs';
+import Help from './screens/Help'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <FormsAndInputs />
+      <Help />
+      {/* <FormsAndInputs /> */}
        {/* <PostList /> */}
       </div>
     );
